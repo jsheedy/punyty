8f14663b12f1399576dd27ea72e4eb3c90e94379
Joseph Sheedy <joseph.sheedy@gmail.com>
2019-05-04 13:12:00 -0700

    array-renderer benchmark

--- demo/array-renderer.py
+++ demo/array-renderer.py
@@ -1,13 +1,16 @@
+""" Doesn't display anything, primarily for benchmarking
+ArrayRenderer independent of display """
+
 import time
 
-import matplotlib
-import matplotlib.pyplot as plt
 import numpy as np
 
 from punyty.vector import Vector3
 from punyty.objects import Cube
 from punyty.renderers import ArrayRenderer
 from punyty.scene import Scene
+
+import benchmark
 
 
 if __name__ == '__main__':
@@ -19,12 +22,12 @@ if __name__ == '__main__':
     scene.add_object(cube)
     renderer = ArrayRenderer(target_array=target_array)
 
-    plt.show()
-    matplotlib.interactive(False)
+    bench = benchmark.Benchmark(renderer)
 
-    for i in range(20):
+    while True:
+        t = time.time()
         renderer.render(scene)
-        plt.imshow(target_array)
-        plt.show()
         cube.rotate(Vector3(time.time(), 0, 0))
-        time.sleep(0.1)
+        fps = bench.update(t)
+        if fps:
+            print(fps)
